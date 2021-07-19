ed0e7a37f14ad4437429bcb19fd31ccfbbc37276
Andrey <kkarelinn@gmail.com>
2021-07-20 00:33:15 +0300

    Update StockServerImpl.java

--- StockServerImpl.java
+++ StockServerImpl.java
@@ -1,51 +1,103 @@
 package socket;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.List;
 
-public class StockServerImpl extends UnicastRemoteObject implements StockServerInt {
-
-
+public class ClientRMI {
 	
-	private String price = null;
-	private ArrayList<String>listSymbols = new ArrayList<>();
-	
-	protected StockServerImpl() throws RemoteException {
-		super();
-		LocateRegistry.createRegistry(1099);
-		//define some stock Symbols code
-		listSymbols.add("AMZN");
-		listSymbols.add("MSFT");
-		listSymbols.add("AAPL");
-		listSymbols.add("YHOO");
-		listSymbols.add("BRNT");
-
 		
-		// TODO Auto-generated constructor stub
-	}
+	static String SYMB;
+	static StockServerInt myServer = null;
+	static String quote;
 
-	@Override
-	public String getQoute(String symbol) throws RemoteException {
-		 if(symbol.equals("exit")) {
+	static OutputStream outbound=null;
+	static BufferedReader inbound=null;
+
+	private static void connectServer() {
+		try {
+			myServer = (StockServerInt)Naming.lookup("rmi://localhost:1099/QService");
+			System.out.println("I have connected to the server");
+			
+
+		}
+		catch(Exception e) {
+			System.out.println("---Failed to connect the server---");
 			System.exit(-1);
-			 return "exit";
-			 
-			 
-		 }
-		
-		if(listSymbols.indexOf(symbol.toUpperCase())!=-1)
-			 price= (new Double(Math.random()*100)).toString();
-		return price;
+		}
+	}
+	
+	private static void getSymbolPrice() {
+		try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));)
+		{	
+			System.out.println("Please, put the Stock symbol to get price");
+			while(true) {
+				SYMB = reader.readLine();
+				if(SYMB.equals("q"))
+				{	
+
+					receivePrice("exit");
+					System.out.println("Exit the programm");
+					reader.close();
+					System.exit(0);
+				}
+
+
+				if(SYMB.equals(""))
+					System.out.println("You no put any symbols");
+
+				if(!SYMB.equals("")) {
+					receivePrice(SYMB);
+				}	
+
+			}
+
+		}
+		catch (Exception e) {
+			System.out.println("You have some problem with inputing symbols");
+		}
+
 	}
 
-	@Override
-	public List<String> getListSymbols() throws RemoteException {
-		
-		return listSymbols;
+	private static void receivePrice(String sym) {
+
+		try {
+
+			if (sym.equals("exit")) {
+				 myServer.getQoute(sym);
+				System.out.println("Server was Shat down");;
+				
+			}
+			else {
+			String price = myServer.getQoute(sym);
+			
+			if(price!=null ) {
+				
+				System.out.println("The price of "+ sym+" - $"+price);
+			}
+			else if (!sym.equals("exit")){
+				System.out.println("Invalid symbolsList. Please, use one of these: "+ myServer.getListSymbols().toString());
+			}
+			}
+
+		} catch (RemoteException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
 	
+	public static void main(String[] args) {
+		connectServer();
+		getSymbolPrice();
+		
+
+		
 	}
 
 }
